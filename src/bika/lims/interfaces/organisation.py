454fafb0e7fe9835dbbacd3adc4b7755102acda9
immunarray <justin.pitts@immunarray.com>
2017-02-02 15:46:11 -0500

    Expanded address fields for organisation, this will allow the data to be used more effectively.

--- src/bika/lims/interfaces/organisation.py
+++ src/bika/lims/interfaces/organisation.py
@@ -25,13 +25,126 @@ class IOrganisation(model.Schema):
         required=False,
     )
 
-    model.fieldset('Address',
-                   label=_(u"Address"),
+    model.fieldset('physicaladdress',
+                   label=_(u"Physical Address"),
                    fields=['physical_address',
-                           'postal_address',
-                           'billing_address',
+                           'physical_address_cont',
+                           'physical_address_city',
+                           'physical_address_state',
+                           'physical_address_zipcode',
+                           'physical_address_country',
                            ]
                    )
+
+    physical_address = schema.TextLine(
+        title=_("Physical address"),
+        required=False,
+    )
+
+    physical_address_cont = schema.TextLine(
+        title=_("Physical address continued"),
+        required=False,
+    )
+
+    physical_address_city = schema.TextLine(
+        title=_("Physical address city"),
+        required=False,
+    )
+
+    physical_address_state = schema.TextLine(
+        title=_("Physical address state"),
+        required=False,
+    )
+
+    physical_address_zipcode = schema.TextLine(
+        title=_("Physical address zipcode"),
+        required=False,
+    )
+
+    physical_address_country = schema.TextLine(
+        title=_("Physical address"),
+        required=False,
+    )
+
+    model.fieldset('postaladdress',
+                   label=_(u"Postal Address"),
+                   fields=['postal_address',
+                           'postal_address_cont',
+                           'postal_address_city',
+                           'postal_address_state',
+                           'postal_address_zipcode',
+                           'postal_address_country',
+                           ]
+                   )
+    postal_address = schema.TextLine(
+        title=_("Postal address"),
+        required=False,
+    )
+
+    postal_address_cont = schema.TextLine(
+        title=_("Postal address continued"),
+        required=False,
+    )
+
+    postal_address_city = schema.TextLine(
+        title=_("Postal address city"),
+        required=False,
+    )
+
+    postal_address_state = schema.TextLine(
+        title=_("Postal address state"),
+        required=False,
+    )
+
+    postal_address_zipcode = schema.TextLine(
+        title=_("Postal address zipcode"),
+        required=False,
+    )
+
+    postal_address_country = schema.TextLine(
+        title=_("Postal address country"),
+        required=False,
+    )
+
+    model.fieldset('billingaddress',
+                   label=_(u"Billing Address"),
+                   fields=['billing_address',
+                           'billing_address_cont',
+                           'billing_address_city',
+                           'billing_address_state',
+                           'billing_address_zipcode',
+                           'billing_address_country',
+                           ]
+                   )
+    billing_address = schema.TextLine(
+        title=_("Billing address"),
+        required=False,
+    )
+
+    billing_address_cont = schema.TextLine(
+        title=_("Billing address continued"),
+        required=False,
+    )
+
+    billing_address_city = schema.TextLine(
+        title=_("Billing address city"),
+        required=False,
+    )
+
+    billing_address_state = schema.TextLine(
+        title=_("Billing address state"),
+        required=False,
+    )
+
+    billing_address_zipcode = schema.TextLine(
+        title=_("Billing address zipcode"),
+        required=False,
+    )
+
+    billing_address_country = schema.TextLine(
+        title=_("Billing address country"),
+        required=False,
+    )
 
     physical_address = schema.Text(
         title=_(u"Physical address"),
